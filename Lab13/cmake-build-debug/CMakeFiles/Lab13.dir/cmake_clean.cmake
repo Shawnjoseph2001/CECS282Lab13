b536c725159ca7c92aacdaf0d4687ee3eefd535d
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2020-11-27 00:49:08 -0800

    /

---
+++ Lab13/cmake-build-debug/CMakeFiles/Lab13.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab13.dir/PointTypeImp.cpp.o"
+  "CMakeFiles/Lab13.dir/circleTypeImp.cpp.o"
+  "CMakeFiles/Lab13.dir/cylinderType.cpp.o"
+  "CMakeFiles/Lab13.dir/main.cpp.o"
+  "Lab13"
+  "Lab13.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab13/cylinderType.cpp
@@ -0,0 +1,48 @@
+//
+// Created by shawn on 11/26/20.
+//
+
+#include "cylinderType.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+void cylinderType::print() const {
+    cout << "Base Center: " << circleType::getX() << ", " << circleType::getY() << endl;
+    cout << "Base radius: " << circleType::getRadius() << endl;
+    cout << "Base circumference: " << circleType::getCircumference() << endl;
+    cout << "Base area: " << circleType::getArea() << endl;
+    cout << "Cylinder Height: " << getHeight() << endl;
+    cout << "Cylinder Surface area: " << getSurfaceArea() << endl;
+    cout << "Cylinder Volume: " << getVolume() << endl;
+}
+
+void cylinderType::setHeight(double h) {
+    height = h;
+}
+
+void cylinderType::setBaseCenter(double x, double y) {
+    circleType::setPoint(x, y);
+}
+
+void cylinderType::setCenterRadiusHeight(double x, double y, double r, double h) {
+    circleType::setPoint(x, y);
+    height = h;
+    circleType::setRadius(r);
+}
+
+double cylinderType::getHeight() const {
+    return height;
+}
+
+double cylinderType::getVolume() const {
+    return getHeight() * circleType::getArea();
+}
+
+double cylinderType::getSurfaceArea() const {
+    return M_PI_4 * circleType::getRadius() * 2 * height;
+}
+
+cylinderType::cylinderType(double x, double y, double r, double h) {
+    setCenterRadiusHeight(x, y, r, h);
+}
+
